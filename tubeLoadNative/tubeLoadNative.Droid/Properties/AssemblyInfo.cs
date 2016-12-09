9a1bb3e4fef8a3c3c4962d5e34e4df38fec24b5c
yonatan kreiner <yonatanKreiner@gmail.com>
2016-12-09 19:01:30 +0200

    deselect proguard and added debbuging false attribute for release

--- tubeLoadNative/tubeLoadNative.Droid/Properties/AssemblyInfo.cs
+++ tubeLoadNative/tubeLoadNative.Droid/Properties/AssemblyInfo.cs
@@ -26,3 +26,10 @@ using Android.App;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+
+// Self implemented attributes
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
