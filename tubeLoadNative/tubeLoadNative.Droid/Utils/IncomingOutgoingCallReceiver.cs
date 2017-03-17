6b0e84136f63411506fd89d5ec71f1093a0bcdd6
Ofir Elarat <Ofir Elarat@DESKTOP-8UTA06E>
2017-03-17 13:31:16 +0200

    outgoing calls pause and start songs fix

--- tubeLoadNative/tubeLoadNative.Droid/Utils/IncomingOutgoingCallReceiver.cs
+++ tubeLoadNative/tubeLoadNative.Droid/Utils/IncomingOutgoingCallReceiver.cs
@@ -6,8 +6,8 @@ using Android.Telephony;
 namespace tubeLoadNative.Droid.Utils
 {
     [BroadcastReceiver()]
-    [IntentFilter(new[] { "android.intent.action.PHONE_STATE" })]
-    public class IncomingCallReceiver : BroadcastReceiver
+    [IntentFilter(new[] { "android.intent.action.PHONE_STATE" ,"android.intent.action.NEW_OUTGOING_CALL"})]
+    public class IncomingOutgoingCallReceiver : BroadcastReceiver
     {
         static bool isPlayed = false;
         public override void OnReceive(Context context, Intent intent)
@@ -38,13 +38,23 @@ namespace tubeLoadNative.Droid.Utils
                 else if (state == TelephonyManager.ExtraStateOffhook)
                 {
                     // incoming call answer
+                    //out going call ring
+                    if (AndroidSongsManager.Instance.CurrentSong != null)
+                    {
+                        if (AndroidSongsManager.Instance.IsPlaying)
+                        {
+                            isPlayed = true;
+                            AndroidSongsManager.Instance.Pause();
+                        }
+                    }
                 }
                 else if (state == TelephonyManager.ExtraStateIdle)
                 {
-                    // incoming call end
+                    // incoming call end and out going call end
                     if (AndroidSongsManager.Instance.CurrentSong != null && isPlayed)
                     {
                         AndroidSongsManager.Instance.Start();
+                        isPlayed = false;
                     }
                 }
             }
