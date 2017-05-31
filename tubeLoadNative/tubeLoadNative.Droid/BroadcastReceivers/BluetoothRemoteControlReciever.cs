80a044ede9186047ce109b83ccb8799415f75a4c
ofirelarat <96ofir11@gmail.com>
2017-05-31 22:09:44 +0300

    fix bluetooth state change

--- tubeLoadNative/tubeLoadNative.Droid/BroadcastReceivers/BluetoothRemoteControlReciever.cs
+++ tubeLoadNative/tubeLoadNative.Droid/BroadcastReceivers/BluetoothRemoteControlReciever.cs
@@ -1,4 +1,5 @@
 using Android.App;
+using Android.Bluetooth;
 using Android.Content;
 using Android.Views;
 
@@ -7,55 +8,74 @@ using tubeLoadNative.Droid.Utils;
 namespace tubeLoadNative.Droid.BroadcastReceivers
 {
     [BroadcastReceiver]
-    [IntentFilter(new[] { Intent.ActionMediaButton })]
+    [IntentFilter(new[] { Intent.ActionMediaButton, BluetoothAdapter.ActionStateChanged, BluetoothAdapter.ActionConnectionStateChanged })]
     public class BluetoothRemoteControlReciever : BroadcastReceiver
     {
         public string ComponentName { get { return Class.Name; } }
         private static bool secondTimeFlag = false;
         public override void OnReceive(Context context, Intent intent)
         {
-            if (!secondTimeFlag && intent.Extras != null)
+            if (intent.Action.Equals(BluetoothAdapter.ActionStateChanged))
             {
-                // get the key event
-                KeyEvent state = (KeyEvent)intent.GetParcelableExtra(Intent.ExtraKeyEvent);
-
-                switch (state.KeyCode)
+                State state = (State)intent.GetIntExtra(BluetoothAdapter.ExtraState, BluetoothAdapter.Error);
+                if (state.Equals(State.TurningOff))
                 {
-                    case Keycode.MediaPlay:
-                        AndroidSongsManager.Instance.Start();
-                        break;
-
-                    case Keycode.MediaStop:
-                        // stop music
-                        AndroidSongsManager.Instance.Stop();
-                        break;
-
-                    case Keycode.Headsethook:
-                    case Keycode.MediaPlayPause:
-                        // pause music
-                        if (AndroidSongsManager.Instance.IsPlaying)
-                        {
-                            AndroidSongsManager.Instance.Pause();
-                        }
-                        else
-                        {
-                            AndroidSongsManager.Instance.Start();
-                        }
-                        break;
-
-                    case Keycode.MediaNext:
-                        // next track
-                        AndroidSongsManager.Instance.PlayNext();
-                        break;
-
-                    case Keycode.MediaPrevious:
-                        // previous track
-                        AndroidSongsManager.Instance.PlayPrev();
-                        break;
+                    AndroidSongsManager.Instance.Pause();
                 }
             }
+            else if (intent.Action.Equals(BluetoothAdapter.ActionConnectionStateChanged))
+            {
+                State state = (State)intent.GetIntExtra(BluetoothAdapter.ExtraConnectionState, BluetoothAdapter.Error);
+                if (state.Equals(State.Disconnected))
+                {
+                    AndroidSongsManager.Instance.Pause();
+                }
+            }
+            else
+            {
+                if (!secondTimeFlag && intent.Extras != null)
+                {
+                    // get the key event
+                    KeyEvent state = (KeyEvent)intent.GetParcelableExtra(Intent.ExtraKeyEvent);
 
-            secondTimeFlag = !secondTimeFlag;
+                    switch (state.KeyCode)
+                    {
+                        case Keycode.MediaPlay:
+                            AndroidSongsManager.Instance.Start();
+                            break;
+
+                        case Keycode.MediaStop:
+                            // stop music
+                            AndroidSongsManager.Instance.Stop();
+                            break;
+
+                        case Keycode.Headsethook:
+                        case Keycode.MediaPlayPause:
+                            // pause music
+                            if (AndroidSongsManager.Instance.IsPlaying)
+                            {
+                                AndroidSongsManager.Instance.Pause();
+                            }
+                            else
+                            {
+                                AndroidSongsManager.Instance.Start();
+                            }
+                            break;
+
+                        case Keycode.MediaNext:
+                            // next track
+                            AndroidSongsManager.Instance.PlayNext();
+                            break;
+
+                        case Keycode.MediaPrevious:
+                            // previous track
+                            AndroidSongsManager.Instance.PlayPrev();
+                            break;
+                    }
+                }
+
+                secondTimeFlag = !secondTimeFlag;
+            }
         }
     }
 }
