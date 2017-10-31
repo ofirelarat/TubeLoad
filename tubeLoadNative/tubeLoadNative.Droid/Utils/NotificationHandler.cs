eb4e18a0ec5a167aaa31819a80efd45b3f7b587b
ofirelarat <96ofir11@gmail.com>
2017-10-31 13:39:00 +0200

    add play apuse icon to the notification

--- tubeLoadNative/tubeLoadNative.Droid/Utils/NotificationHandler.cs
+++ tubeLoadNative/tubeLoadNative.Droid/Utils/NotificationHandler.cs
@@ -5,6 +5,8 @@ using Android.Content;
 using Android.Media;
 using tubeLoadNative.Droid.Activities;
 using Android.Views;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 
 namespace tubeLoadNative.Droid.Utils
 {
@@ -42,9 +44,20 @@ namespace tubeLoadNative.Droid.Utils
             }
 
             builder.SetSmallIcon(Resource.Drawable.icon);
+            Drawable drawable = SongMetadata.GetSongPicture(songId);
+            Bitmap bitmap;
+            if (drawable != null)
+            {
+                bitmap = ((BitmapDrawable)SongMetadata.GetSongPicture(songId)).Bitmap;
+            }
+            else
+            {
+                bitmap = BitmapFactory.DecodeResource(Application.Context.Resources, Resource.Drawable.default_song_image);
+            }
+
+            builder.SetLargeIcon(bitmap);
             builder.SetContentTitle(title);
             builder.SetContentText(content);
-
 
             songNotification = builder.Build();
 
@@ -61,19 +74,19 @@ namespace tubeLoadNative.Droid.Utils
             Intent prevIntent = new Intent(Intent.ActionMediaButton);
             prevIntent.PutExtra(Intent.ExtraKeyEvent, new KeyEvent(KeyEventActions.Down, Keycode.MediaPrevious));
             PendingIntent prevPendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, prevIntent, PendingIntentFlags.UpdateCurrent);
-            Notification.Action prevAction = new Notification.Action(Resource.Drawable.ic_media_previous, "", prevPendingIntent);
+            Notification.Action prevAction = new Notification.Action(Resource.Drawable.ic_media_previous, string.Empty, prevPendingIntent);
             builder.AddAction(prevAction);
 
             Intent playPauseIntent = new Intent(Intent.ActionMediaButton);
             playPauseIntent.PutExtra(Intent.ExtraKeyEvent, new KeyEvent(KeyEventActions.Down, Keycode.MediaPlayPause));
             PendingIntent playPausePendingIntent = PendingIntent.GetBroadcast(Application.Context, 1, playPauseIntent, PendingIntentFlags.UpdateCurrent);
-            Notification.Action playPauseAction = new Notification.Action(Resource.Drawable.ic_media_pause, "", playPausePendingIntent);
+            Notification.Action playPauseAction = new Notification.Action(Resource.Drawable.ic_media_play_pause, string.Empty, playPausePendingIntent);
             builder.AddAction(playPauseAction);
 
             Intent nextIntent = new Intent(Intent.ActionMediaButton);
             nextIntent.PutExtra(Intent.ExtraKeyEvent, new KeyEvent(KeyEventActions.Down, Keycode.MediaNext));
             PendingIntent nextPendingIntent = PendingIntent.GetBroadcast(Application.Context, 2, nextIntent, PendingIntentFlags.UpdateCurrent);
-            Notification.Action nextAction = new Notification.Action(Resource.Drawable.ic_media_next, "", nextPendingIntent);
+            Notification.Action nextAction = new Notification.Action(Resource.Drawable.ic_media_next, string.Empty, nextPendingIntent);
             builder.AddAction(nextAction);
         }
     }
